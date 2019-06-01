6b3d1f06fd6fb6f89b16cb0ae562cd0c31ab943a
Lenox Jung <lenoxjung@gmail.com>
2019-06-01 00:08:42 +0000

    added comment and changed readme

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
 # Web-scraper modified to output SQL insert statements
 # Instructions
+npm install
+
 node scraper-runner.js to run all three web-scrapers to write onto same txt
+
+set up database
+
+CREATE TABLE Locations (
+  locationId SERIAL PRIMARY key,
+  name text,
+  region text,
+  description text,
+  locationLat text,
+  locationLong text,
+  phoneNumber text,
+  website text,
+  postalCode text,
+  cityName text,
+  streetName text,
+  address text,
+  hoursMon text,
+  hoursTue text,
+  hoursWed text,
+  hoursThur text,
+  hoursFri text,
+  hoursSat text,
+  hoursSun text  
+);
+
+copy and paste contents of insert-statements.txt and run to format table
+
+then view table:
+SELECT name, region, phoneNumber, postalCode, cityName, streetName, address FROM Locations;
--- aglc-scraper.js
+++ aglc-scraper.js
@@ -39,7 +39,7 @@ rp(url)
                         case 9:
                             data.phoneNumber = cellData;
                             info.push(data); // phone number is the last piece of info for the licensee
-                            fileData += `  ('${data.licenseeName.replace('\'','\'\'')}', '${data.region}', '${data.phoneNumber}', '${data.postalCode}', '${data.city}', '${data.address}', '${data.address}, ${data.city}, ${data.region} ${data.postalCode}'),\n`; // insert value statement with newline
+                            fileData += `  ('${data.licenseeName.replace('\'','\'\'')}', '${data.region}', '${data.phoneNumber}', '${data.postalCode}', '${data.city}', '${data.address}', '${data.address}, ${data.city}, ${data.region} ${data.postalCode}'),\n`; // replace single quote with two quotes to ensure it escapes in SQL
                             break;
                         default:
                             break;
